bcca70c3a025afd9cca900231e47129622ce08ef
leonardhere <44140039+leonardhere@users.noreply.github.com>
2023-06-02 18:12:03 +0300

    -

--- pizza/src/redux/actions/filters.js
+++
@@ -1,9 +0,0 @@
-export const setSortBy = ({ type, order }) => ({
-  type: 'SET_SORT_BY',
-  payload: { type, order },
-});
-
-export const setCategory = (catIndex) => ({
-  type: 'SET_CATEGORY',
-  payload: catIndex,
-});
