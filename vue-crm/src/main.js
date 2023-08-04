9e9ffbc7e465f19da2cb851a7671e130e870dcc3
leonardhere <44140039+leonardhere@users.noreply.github.com>
2023-08-04 15:11:00 +0300

    crm

---
+++ vue-crm/src/main.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import titlePlugin from '@/utils/title.plugin'
+import Loader from '@/components/app/Loader'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(titlePlugin)
+Vue.use(Vuelidate)
+Vue.use(VueMeta)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDAPqALfM3xjg7II0tjg7GL6s73mKXRxEs',
+  authDomain: 'vue-crm-e3928.firebaseapp.com',
+  databaseURL: 'https://vue-crm-e3928.firebaseio.com',
+  projectId: 'vue-crm-e3928',
+  storageBucket: 'vue-crm-e3928.appspot.com',
+  messagingSenderId: '251307374581'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
